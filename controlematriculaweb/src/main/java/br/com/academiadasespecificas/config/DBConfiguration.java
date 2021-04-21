72ae86a9ba519f8c8f273465f0213254fb2170a5
Pedro Brandão <pedro@pedrobrandao.com.br>
2021-04-21 11:50:32 -0300

    Submete mudanças pendentes para formatar

---
+++ controlematriculaweb/src/main/java/br/com/academiadasespecificas/config/DBConfiguration.java
@@ -0,0 +1,39 @@
+package br.com.academiadasespecificas.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@ConfigurationProperties("sprint.datasource")
+@Getter
+@Setter
+public class DBConfiguration {
+
+    private String driverClassName;
+    private String url;
+    private String userName;
+    private String password;
+
+    @Profile("dev")
+    @Bean
+    public String testDatabaseConnection(){
+        System.out.println("DB connection for DEV - H2");
+        System.out.println(driverClassName);
+        System.out.println(url);
+        return "DB Connection to H2_TEST - Test instance";
+    }
+
+    @Profile("prod")
+    @Bean
+    public String productionDatabaseConnection(){
+        System.out.println("DB connection for Production - MySQL");
+        System.out.println(driverClassName);
+        System.out.println(url);
+        return "DB Connection to MYSQL_PROD - Test instance";
+    }
+
+}
